053f6803e13a40a76201eae134efeb80d6783311
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2020-11-11 16:33:25 -0800

    displaying the menu

--- src/ClientA/client_send.py
+++ src/ClientA/client_send.py
@@ -1,4 +1,5 @@
 from socket import *
+
 severName = 'localhost'
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
@@ -9,18 +10,34 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 # clientSocket.close()
 
 choice = input("""
-  1:"Enter a new transaction."
-  2:"The current balance for each account."
-  3:"Print the unconfirmed transactions."
-  4:"Print the last X number of confirmed transactions (either as a Payee or a Payer)."
-  5:"Print the blockchain."
-  
-  Please enter your choice:""")
+1:Enter a new transaction.
+2:The current balance for each account.
+3:Print the unconfirmed transactions.
+4:Print the last X number of confirmed transactions (either as a Payee or a Payer).
+5:Print the blockchain.
+
+Please enter your choice:""")
+
+
+def display_functions():
+    print("Select the payer:")
+    choice_payer = input("""
+   1:A0000001
+   2:A0000002
+       """)
+
+    if choice_payer == "1" or choice_payer == "2":
+        print("Select the payee:")
+        choice_payee = input("""
+    1:B0000001
+    2:B0000002
+            """)
+
 
 
 def new_transaction():
-    pass
-
+    display_functions()
+    amount = input("Enter the amount in decimal:")
 
 def current_balance():
     pass
@@ -38,13 +55,13 @@ def blockchain():
     pass
 
 
-if choice == "A":
+if choice == "1":
     new_transaction()
-elif choice == "B":
+elif choice == "2":
     current_balance()
-elif choice == "B":
+elif choice == "3":
     unconfirmed_transactions()
-elif choice == "B":
+elif choice == "4":
     confirmed_transactions()
-elif choice == "B":
+elif choice == "5":
     blockchain()
