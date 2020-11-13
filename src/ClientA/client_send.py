0a23d5c98903e05887881c868133ba6d0016ff4a
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2020-11-12 17:54:14 -0800

    current-balance

--- src/ClientA/client_send.py
+++ src/ClientA/client_send.py
@@ -9,6 +9,17 @@ clientSocket = socket(AF_INET, SOCK_DGRAM)
 # print(modifiedMessage.decode())
 # clientSocket.close()
 
+
+# initializing-balances
+uinitial_balance = int(1000)
+cinitial_balance = int(1000)
+aer = hex(uinitial_balance)
+aer1 = hex(cinitial_balance)
+with open('balance', 'w') as g:
+    g.writelines("A0000001: " + aer + " : " + aer1 + "\n")
+    g.writelines("A0000002: " + aer + " : " + aer1)
+
+
 choice = input("""
 1:Enter a new transaction.
 2:The current balance for each account.
@@ -17,8 +28,6 @@ choice = input("""
 5:Print the blockchain.
 
 Please enter your choice:""")
-
-
 
 
 def new_transaction():
@@ -33,19 +42,45 @@ def new_transaction():
 1:B0000001
 2:B0000002
             """)
-    amount = input("Enter the amount in decimal:")
-    Tx_amount = hex(amount)
+
+    # converting the tx_amount into hex#
+    amount = input("Enter the amount of payment in decimal:")
+    i_amount = int(amount)
+    tx_amount = hex(i_amount)
 
 
-# if choice_payer == "1":
+    # tx-fee
+    tx_fee = hex(2)
+
+    if choice_payer == "1":
+        with open('balance', 'r') as t:
+            f_content1 = t.readline()
+            f_content2 = f_content1.split(":")
+            var = f_content2[1]  # unconfirmed_balance = var
+
+            tm = tx_amount + tx_fee
+
+        if tm <= var:
+            var -= tm
+            with open('Unconfirmed_T', 'w') as w:
+                w.write('h')
 
 
 def current_balance():
     with open('balance', 'r') as f:
         f_content = f.readline()
-        print("Account-1 " + f_content[18:26])
+        con = f_content.split(":")
+        var = con[2]
+        var1 = int(var, 16)
+        final = str(var1)
+        print("Account-1 : " + final)
+
         f_content = f.readline()
-        print("Account-2 " + f_content[18:26])
+        con1 = f_content.split(":")
+        var2 = con1[2]
+        var3 = int(var2, 16)
+        final1 = str(var3)
+        print("Account-2 : " + final1)
 
 
 def unconfirmed_transactions():
